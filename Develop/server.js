abf5a65c1299fc73c6d20356e20d79a84c892eaa
Dina <diana.liubarets@hotmail.com>
2020-07-27 17:07:20 -0400

    first commit

---
+++ Develop/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+var app = express();
+let db = require("./db/db.json");
+let uuid = require("uuid");
+const fs = require("fs");
+var path = require("path");
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//routes
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  console.log("/api/notes-get");
+  res.json(db)
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+//api
+
+app.post("/api/notes", function (req, res) {
+  console.log("/api/notes-post");
+
+  let note = req.body;
+  note.id = uuid.v1();
+  db.push(note);
+  writeToDB(db);
+
+  return res.json(db);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  console.log("/api/notes-delete");
+
+  let id = req.params.id;
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].id == id) {
+      db.splice(i, 1);
+      writeToDB(db);
+      res.json(db)
+      break;
+    }
+  }
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
+
+function writeToDB(array) {
+  fs.writeFileSync("./db/db.json", JSON.stringify(array));
+}
